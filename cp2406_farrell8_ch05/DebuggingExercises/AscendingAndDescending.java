59845429d1545ba3edb6247cc96f91b84db70432
ryant <ryan.thorp@my.jcu.edu.au>
2016-09-09 16:31:13 +1000

    fixes

--- cp2406_farrell8_ch05/DebuggingExercises/AscendingAndDescending.java
+++ cp2406_farrell8_ch05/DebuggingExercises/AscendingAndDescending.java
@@ -5,16 +5,41 @@ import java.util.Scanner;
  */
 public class AscendingAndDescending {
     public static void main(String[] args) {
+        int low = 0, med = 0, high = 0;
+        int num1, num2, num3;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number");
-        int num1 = input.nextInt();
+        num1 = input.nextInt();
         System.out.println("Enter the second number");
-        int num2 = input.nextInt();
+        num2 = input.nextInt();
         System.out.println("Enter the third number");
-        int num3 = input.nextInt();
-        if (num1 < num2 && num1 < num3){
-            System.out.println(num1);
+        num3 = input.nextInt();
+        if (num1 < num2 && num1 < num3) {
+            low = num1;
+            if (num2 < num3) {
+                med = num2;
+                high = num3;
+            } else {
+                med = num3;
+                high = num2;
+            }
         }
+        else {
+            if (num2 < num1 && num2 < num3) {
+                low = num2;
+                if (num1 < num2) {
+                    med = num1;
+                    high = num3;
+                } else {
+                    med = num2;
+                    high = num1;
+                }
+
+            }
+
+        }
+        System.out.println("Ascending - " + low + " " + med + " " + high);
+        System.out.println("Descending - " + high + " " + med + " " + low);
 
     }
 }
